a0fbedcb3abdaec50e26bca22318f50ff7b26424
Gonzalo <gonespgra@gmail.com>
2024-10-15 17:13:08 -0700

    slay

--- Troubleshooting/Sep20_2024/leg_full_sweep.ino
+++ Troubleshooting/Sep20_2024/leg_full_sweep.ino
@@ -19,25 +19,25 @@ void setup() {
 }
 
 void loop() {
-  // // Sweep through Servo 2 angles with Servo 1 minimum angles
-  // for (int i = 0; i < 18; i++) {
-  //   servo2.write(servo2Angles[i]);     // Set Servo 2 to the current angle
-  //   servo1.write(servo1MinAngles[i]);  // Set Servo 1 to the corresponding minimum angle
-  //   Serial.print("Servo 2 Angle: ");
-  //   Serial.print(servo2Angles[i]);
-  //   Serial.print(" | Servo 1 Min Angle: ");
-  //   Serial.println(servo1MinAngles[i]);
-  //   delay(20);
-  // }
+  // Sweep through Servo 2 angles with Servo 1 minimum angles
+  for (int i = 0; i < 18; i++) {
+    servo2.write(servo2Angles[i]);     // Set Servo 2 to the current angle
+    servo1.write(servo1MinAngles[i]);  // Set Servo 1 to the corresponding minimum angle
+    Serial.print("Servo 2 Angle: ");
+    Serial.print(servo2Angles[i]);
+    Serial.print(" | Servo 1 Min Angle: ");
+    Serial.println(servo1MinAngles[i]);
+    delay(20);
+  }
 
-  // // Sweep through Servo 2 angles with Servo 1 maximum angles
-  // for (int i = 0; i < 18; i++) {
-  //   servo2.write(servo2Angles[i]);     // Set Servo 2 to the current angle
-  //   servo1.write(servo1MaxAngles[i]);  // Set Servo 1 to the corresponding maximum angle
-  //   Serial.print("Servo 2 Angle: ");
-  //   Serial.print(servo2Angles[i]);
-  //   Serial.print(" | Servo 1 Max Angle: ");
-  //   Serial.println(servo1MaxAngles[i]);
-  //   delay(20);
-  // }
+  // Sweep through Servo 2 angles with Servo 1 maximum angles
+  for (int i = 0; i < 18; i++) {
+    servo2.write(servo2Angles[i]);     // Set Servo 2 to the current angle
+    servo1.write(servo1MaxAngles[i]);  // Set Servo 1 to the corresponding maximum angle
+    Serial.print("Servo 2 Angle: ");
+    Serial.print(servo2Angles[i]);
+    Serial.print(" | Servo 1 Max Angle: ");
+    Serial.println(servo1MaxAngles[i]);
+    delay(20);
+  }
 }
